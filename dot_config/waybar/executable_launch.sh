4cd1b454aa3ede982fa02d0ddd5a7fac917446f1
Geoff Garside <geoff@geoffgarside.co.uk>
2024-08-29 14:36:37 +0100

    waybar: cleanup and fixes
    * use catppuccin-mocha theme file
    * fix launch script
    * fix reference to check-updates script
    * style custom-updates element

--- dot_config/waybar/executable_launch.sh
+++ dot_config/waybar/executable_launch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+DIR="$(dirname "${BASH_SOURCE[@]}")"
 PIDS="$(pidof waybar)"
 if [ -n "${PIDS}" ]; then
 	kill -9 ${PIDS}
 	sleep 0.2
-done
+fi
 
-waybar -c config.jsonc -s style.css &
+waybar -c "${DIR}/config.jsonc" \
+	-s "${DIR}/style.css" \
+	-l error &
